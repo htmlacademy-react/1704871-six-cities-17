0cfea3c953485e7f9288feaea606e3d5c56df4f0
dmoldd <oldtime_tula@mail.ru>
2024-12-01 22:03:13 +0300

    вносит еще исправления линтера

--- src/const.ts
+++ src/const.ts
@@ -5,8 +5,31 @@ const IS_LOGGED = true;
 const RATING_RATIO = 20;
 const CURRENT_CITY = 'Paris';
 const RATING_STARS_COUNT = 5;
-const NEIGHBORHOOD_PLACES = 3;
+const NEIGHBOURHOOD_PLACES = 3;
 const GALLERY_IMAGE_COUNT = 6;
+
+const CITIES = [
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Amsterdam',
+  'Hamburg',
+  'Dusseldorf',
+] as const;
+
+const AppRoute = {
+  Main: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+  Offer: '/offer/:id',
+  NotFoundPage: '*',
+} as const;
+
+const AuthStatus = {
+  Auth: 'AUTH',
+  NoAuth: 'NO_AUTH',
+  Unknown: 'UNKNOWN',
+} as const;
 
 export {
   FAVORITE_COUNT,
@@ -16,6 +39,9 @@ export {
   RATING_RATIO,
   CURRENT_CITY,
   RATING_STARS_COUNT,
-  NEIGHBORHOOD_PLACES,
+  NEIGHBOURHOOD_PLACES,
   GALLERY_IMAGE_COUNT,
+  CITIES,
+  AppRoute,
+  AuthStatus,
 };
